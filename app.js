516fa6ab931fbc0330f5ba6c1bc6ad272cd49135
Kate Bryan <katebryan31@gmail.com>
2021-06-24 15:20:50 +0100

    feat: added attack method

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const app = Vue.createApp({
   data() {
     return {
-        playerHealth: 100,
-        monsterHealth: 100
+      playerHealth: 100,
+      monsterHealth: 100,
     };
+  },
+  methods: {
+    attackMonster() {
+      // formula for calculating a random number between 12 and 5
+      const attackValue = Math.floor(Math.random() * (12 - 5) + 5);
+      this.monsterHealth -= attackValue;
+    },
   },
 });
 
