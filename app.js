b2dbecef84d18afddec62e6f51b8777c004cdd81
Kate Bryan <katebryan31@gmail.com>
2021-06-26 16:48:31 +0100

    chore: battleLog formatting

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   computed: {
@@ -50,6 +51,7 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
     surrender() {
       this.winner = "monster";
@@ -58,16 +60,19 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 12);
+      this.addLogMessage('monster', 'attack', attackValue)
       this.playerHealth -= attackValue;
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     healPlayer() {
@@ -78,7 +83,15 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue)
       this.attackPlayer();
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
